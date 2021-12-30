929872f7f6f9f01e5735e08e0a63bc3f868dd076
Ubuntu <ubuntu@ip-172-31-82-105.ec2.internal>
2021-12-30 05:12:10 +0000

    done

--- Dice.java
+++ Dice.java
@@ -6,7 +6,7 @@
 * @author Jakob
 * @version 1.0
 * @since 2020-11-26
-* 
+*
 */
 
 import java.util.Random;
@@ -17,9 +17,29 @@ import java.util.Scanner;
 */
 final class Dice {
 
-public static final int MAX = 6;
-public static final int MIN = 1;
-public static final int NUM = 100;
+    /** Constant. */
+    public static final int MAX = 6;
+
+    /** Constant. */
+    public static final int MIN = 1;
+
+    /** Constant. */
+    public static final int NUM = 100;
+
+    /** Constant. */
+    public static final int TWO = 2;
+
+    /** Constant. */
+    public static final int THREE = 3;
+
+    /** Constant. */
+    public static final int FOUR = 4;
+
+    /** Constant. */
+    public static final int FIVE = 5;
+
+    /** Constant. */
+    public static final String DONE = "\nDone.";
 
     /**
     * Prevents instantiation.
@@ -41,52 +61,52 @@ public static final int NUM = 100;
 
     public static void main(final String[] args) {
 
-
-        int[] numbers = new int[] { 1, 2, 3, 4, 5, 6 };
+        final int[] numbers = new int[] {MIN, TWO, THREE, FOUR, FIVE, MAX };
         final int cMove = numbers[new Random().nextInt(numbers.length)];
 
         final Scanner scanner = new Scanner(System.in);
-        int  pMove;
-try {
-        System.out.println("Pick a number between 1-6: ");
-        pMove = scanner.nextInt();
-        int numberOfTries = 1;
-int counter = 1;
-while (counter > NUM) {
-counter = counter + 1;
-}
+        int pMove;
 
-    do {
-            
-   numberOfTries = numberOfTries + 1;
-    
-    if (pMove > MAX || pMove < MIN) {
-    System.out.println("Guess won't count.");
-}
-    else if (pMove < cMove) {
-    System.out.println("Too low!");
-}
-    else if (pMove > cMove) {
-    System.out.println("Too high!");
-}
-    else {
-    System.out.println("You Guessed Correctly! It took you 1 try.");
-System.out.println("\nDone.");
-System.exit(0);
-}
+        try {
+            System.out.println("Pick a number between 1-6: ");
+            pMove = scanner.nextInt();
+            int numberOfTries = 1;
+            int counter = 1;
+            while (counter > NUM) {
+                counter = counter + 1;
+            }
 
-System.out.println("Keep guessing");
-pMove = scanner.nextInt();
-}
-while (pMove != cMove);
+            do {
+
+                numberOfTries = numberOfTries + 1;
+
+                if (pMove > MAX || pMove < MIN) {
+                    System.out.println("Guess won't count.");
+                }
+                else if (pMove < cMove) {
+                    System.out.println("Too low!");
+                }
+                else if (pMove > cMove) {
+                    System.out.println("Too high!");
+                }
+                else {
+                    System.out.println("You Guessed Correctly! It took you 1 try.");
+                    System.out.println(DONE);
+                    System.exit(0);
+                }
+
+                System.out.println("Keep guessing");
+                pMove = scanner.nextInt();
+
+            } while (pMove != cMove);
 
             System.out.println("You Guessed Correctly! It took you "
- + numberOfTries + " tries.");
-}
-catch (java.util.InputMismatchException errorCode) {
+                + numberOfTries + " tries.");
+        }
+        catch (java.util.InputMismatchException errorCode) {
             // Invalid Input.
             System.out.println("This is not an integer.");
-}
-System.out.println("\nDone.");
-}
+        }
+        System.out.println(DONE);
+    }
 }
